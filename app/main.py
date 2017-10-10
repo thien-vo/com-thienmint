763bb74bb6b766f4dbb6aa1af57d6b6b04f37cd3
Thien Vo <jaysonvo97@gmail.com>
2017-10-09 22:06:13 -0500

    change project structure to work well with appengine

--- app/main.py
+++ app/main.py
@@ -11,14 +11,4 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 # [END create_app]
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("error.html")
+from app import views

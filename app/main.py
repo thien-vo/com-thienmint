8f1595e0a7a52c7995b90d8f548fb102c1b86b62
Thien Vo <jaysonvo97@gmail.com>
2017-10-17 21:17:26 -0500

    switching to use cevee template

--- app/main.py
+++ app/main.py
@@ -8,17 +8,13 @@ from flask import Flask, render_template, request
 # [END imports]
 
 # [START create_app]
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 # [END create_app]
 
 # [START routing]
 @app.route('/')
 def index():
-    return render_template("index.html")
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
+    return render_template("home.html")
 
 @app.errorhandler(404)
 def page_not_found(e):

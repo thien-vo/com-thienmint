8e788667bbccda1c72222859720c6a75379ecb17
Thien Vo <jaysonvo97@gmail.com>
2017-11-14 13:14:39 -0600

    Frontend/input personal information (#3)
    * fix typo and remove empty line
    
    * adjust about portion of the home page
    
    * add responsive sliders css and js
    
    * css and js for responsive sliders
    
    * add skill gauge array, remove call-to-action
    
    * add quotes slider
    
    * add extra information
    
    * remove typo
    
    * adjust contact message size to be smaller
    
    * adjust contact form to be more visually static
    
    * add todo for implementation of send_message

--- app/main.py
+++ app/main.py
@@ -14,8 +14,18 @@ app = Flask(__name__, static_url_path='/static')
 # [START routing]
 @app.route('/')
 def home():
-
     return render_template("home.html")
+
+
+@app.route('/send_message', methods=['GET', 'POST'])
+def send_message():
+    print(request.form['contactName'])
+    print(request.form['contactEmail'])
+    print(request.form['contactSubject'])
+    print(request.form['contactMessage'])
+    # TODO: Implement email
+    return "Gotcha"
+
 
 @app.errorhandler(404)
 def page_not_found(e):

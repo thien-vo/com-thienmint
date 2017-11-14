2f7da60e9a18baa7a22cc201b59a8c265ac37791
Thien Vo <jaysonvo97@gmail.com>
2017-11-13 20:03:56 -0600

    Platform/travis ci (#2)
    * travis-ci credentials for gclodu service account
    
    * deployment scripts
    
    * trying to deploy on to production site via this branch
    
    * change back to only deploy to master
    
    * experiment with before_deployment script, and also try to deploy via this branch again
    
    * deployment to this branch
    
    * add on branches to before_deploy
    
    * remove provider script
    
    * adjust the name of json
    
    * add a space between iv and -in
    
    * adjust file to match
    
    * adjust template to be more generic

---
+++ scripts/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+echo "START PRE-DEPLOYMENT: Authentication"
+echo "CURRENT PATH: `pwd`"
+
+# [START auth]
+# Decrypt the credentials we added to the repo using the key we added with the Travis command line tool
+  openssl aes-256-cbc -K $encrypted_47749fd3305f_key -iv $encrypted_47749fd3305f_iv -in credentials.tar.gz.enc -out credentials.tar.gz -d
+
+  tar -xzf credentials.tar.gz
+  echo "MAKING SURE THAT THE KEYS ARE THERE"
+  ls -l client-secret.json
+# [END auth]
+
+# Set the correct project to deploy to
+if [ "$1" == "master" ]; then
+  echo "Authenticating with master project"
+  gcloud auth activate-service-account --key-file client-secret.json
+  echo "Set deployment to master"
+  gcloud config set project python-181206
+fi
+
+ echo "START DEPLOYMENT: deploy.sh"
+ echo "CURRENT PATH: `pwd`"
+
+ cd app
+ echo "Inside `pwd` now"
+    echo "---- Installing the Python dependencies"
+    pip install -r requirements.txt -t lib/
+    echo "---- Deployment starts"
+    gcloud app deploy app.yaml
+    echo "---- Deployment ends"
+ cd ..
+ echo "Back in `pwd` directory now"

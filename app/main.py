9f8946daaaf4c8714babc131f0631533b4ee16af
Thien Vo <jaysonvo97@gmail.com>
2017-11-15 00:03:57 -0600

    finished implementing email functionalities

--- app/main.py
+++ app/main.py
@@ -1,57 +1,57 @@
 #main.py
 
-# [START app]
-import logging
-
 # [START imports]
-from flask import Flask, render_template, request
-from google.cloud import datastore
-from google.auth.exceptions import DefaultCredentialsError
+import logging, traceback
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message
+from email_info import get_info
 # [END imports]
 
 # [START create_app]
 app = Flask(__name__, static_url_path='/static')
+app.config.from_pyfile('config.py')
+email_username, email_password = get_info()
+app.config['MAIL_USERNAME'] = email_username
+app.config['MAIL_PASSWORD'] = email_password
+mail = Mail(app)
 # [END create_app]
 
 # [START routing]
+
+
 @app.route('/')
 def home():
     return render_template("home.html")
 
 
-@app.route('/send_message', methods=['GET', 'POST'])
+@app.route('/send_message', methods=['POST'])
 def send_message():
-    print(request.form['contactName'])
-    print(request.form['contactEmail'])
-    print(request.form['contactSubject'])
-    print(request.form['contactMessage'])
-    # TODO: Implement email
-    return "Gotcha"
+    if email_username is None or email_password is None:
+        return "Something went wrong. Please try again later or send an email to thienqvo@gmail.com!"
 
+    contact_name = request.form['contactName']
+    contact_subject = request.form['contactSubject']
+    contact_message = "Received from: {0}\n{1}".format(request.form['contactEmail'], request.form['contactMessage'])
 
-@app.route('/datastore')
-def gcloud():
-    # Instantiates a client
+    if contact_subject is None or contact_subject == "":
+        contact_subject = "Message sent from thienmint.com"
+
+    msg = Message(subject=contact_subject,
+                  body=contact_message,
+                  sender=(contact_name, email_username),
+                  recipients=["thienqvo@gmail.com"])
     try:
-        datastore_client = datastore.Client('python-181206')
-    except DefaultCredentialsError:
-        return "Run into authentication problem"
+        mail.send(msg)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        return "Couldn't send email. Please try again later or send an email to thienqvo@gmail.com!"
 
-    # The kind for the new entity
-    kind = 'Task'
-    # The name/ID for the new entity
-    name = 'sampletask3'
-    # The Cloud Datastore key for the new entity
-    task_key = datastore_client.key(kind, name)
+    return "OK"
 
-    # Prepares the new entity
-    task = datastore.Entity(key=task_key)
-    task['description'] = u"Buy milk"
 
-    # Saves the entity
-    datastore_client.put(task)
-
-    return 'Saved {}: {}'.format(task.key.name, task['description'])
+@app.route('/send_message', methods=['GET', 'PUT', 'DELETE'])
+def redirect_contact():
+    return redirect(url_for('home', _anchor='contact'))
 
 
 @app.errorhandler(404)
